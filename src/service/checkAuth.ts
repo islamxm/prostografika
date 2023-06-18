f4aa29c8c52b5794b5d4b28833ae58195d466148
Islam <isxmad@gmail.com>
2023-06-18 06:09:09 +0300

    check auth

--- src/service/checkAuth.ts
+++ src/service/checkAuth.ts
@@ -3,6 +3,7 @@ import { Cookies } from "typescript-cookie";
 const checkAuth = (res: Response) => {
     if(res?.status === 401) {
         Cookies.remove('prostografika-token')
+        window.location.replace(window.location.origin + '/auth')
     } else {
         return res?.json()
     }
