1874f74b533052c7c3f24022d678081c3035648d
Baymurat <baymurat.orazov@mail.ru>
2023-08-07 16:38:33 +0500

    feat: display templates preview image

--- src/pages/tempsPage/components/TempCard/TempCard.tsx
+++ src/pages/tempsPage/components/TempCard/TempCard.tsx
@@ -1,12 +1,14 @@
+import { TTemplate } from '@store/initState';
+
 import styles from './TempCard.module.scss';
 
-
-
-const TempCard = () => {
+const TempCard = (props: TTemplate) => {
 
   return (
     <div className={styles.wrapper}>
-
+      {(props.type === 'image' || props.type === 'generated')
+        ? <img src={`data:image/png;base64,${props.image}`} />
+        : <div className={styles.selectColor}>Выбрать цвет</div>}
     </div>
   );
 };
