7744206237a252198264d166c72a4a021388e0b4
Baymurat <baymurat.orazov@mail.ru>
2023-08-08 13:33:41 +0500

    feat: update craco config

--- craco.config.js
+++ craco.config.js
@@ -10,6 +10,7 @@ module.exports = {
       '@hooks': path.resolve(__dirname, 'src/hooks'),
       '@store': path.resolve(__dirname, 'src/store'),
       '@utils': path.resolve(__dirname, 'src/utils'),
+      '@icons': path.resolve(__dirname, 'src/icons'),
     },
     configure: {
       devtool: "eval-source-map",
