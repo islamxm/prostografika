89331cf7a0819ff1543a7f846d0009157f4e14c9
Baymurat <baymurat.orazov@mail.ru>
2023-08-09 16:51:02 +0500

    refactor: format ChechAuth component

--- src/hoc/CheckAuth.tsx
+++ src/hoc/CheckAuth.tsx
@@ -1,23 +1,15 @@
 import { useAppSelector } from "@hooks/reduxHooks";
+import { FC, PropsWithChildren } from 'react';
 import { Navigate } from "react-router-dom";
 
-const CheckAuth = ({
-  children
-}: {
-  children?: React.ReactNode
-}) => {
-  const { mainReducer: token } = useAppSelector(s => s);
-
-
-
+const CheckAuth: FC<PropsWithChildren> = ({ children }) => {
+  const { mainReducer: { token } } = useAppSelector(s => s);
 
   if (token) {
     return <>{children}</>;
   }
 
   return <Navigate to={'/auth'} />;
-
 };
-
 
 export default CheckAuth;
