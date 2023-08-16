d1c0cd6b71209d9b3c6a07bc835616c0898235b9
Baymurat <baymurat.orazov@mail.ru>
2023-08-16 15:29:37 +0500

    refactor: clean up test data

--- src/pages/editCardPage/index.tsx
+++ src/pages/editCardPage/index.tsx
@@ -1,40 +1,22 @@
 import Button from '@components/Button/Button';
 import ColorEditor from '@components/color-editor';
 import Headline from '@components/Headline/Headline';
-import ImageEditor from '@components/image-editor';
 import SvgEditor from '@components/svg-editor';
 import TextCreator from '@components/text-creator';
 import TextEditor from '@components/text-editor';
 import { useCustomRef } from '@hooks/customRef';
 import { useAppDispatch, useAppSelector } from '@hooks/reduxHooks';
-import iconCross from '@icons/icon-cross.png';
-import MainApi from '@service/MainApi';
-import { main_updateMarketId, setCardSize } from '@store/slices/mainSlice/mainSlice';
+import { setCardSize } from '@store/slices/mainSlice/mainSlice';
 import { createDeleteControl } from '@utils/fabricControls';
 import { fabric } from 'fabric';
-import { IEvent, Image, Transform } from 'fabric/fabric-impl';
+import { IEvent, Image } from 'fabric/fabric-impl';
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 
 import styles from './styles.module.scss';
 
-// const service = new MainApi();
-
-// const chairBase64 = service.getChairBase64();
-// const bgBase64 = service.getTestBgBase64();
-// const bgBase641 = service.getTestBgBase641();
-// const bgBase642 = service.getTestBgBase642();
-// const brainSVG = service.getBrainBase64();
-
 const EditCardPage = () => {
   const dispatch = useAppDispatch();
-  // -----------------------TEMP-START----------------------------
-  // const { markets } = useAppSelector(s => s.mainReducer);
-
-  // useEffect(() => {
-  //   dispatch(main_updateMarketId(1));
-  // }, [markets]);
-  // -----------------------TEMP-END----------------------------
 
   const [canvasContainerRef, setCanvasContainerRef] = useCustomRef<HTMLDivElement>();
   const [canvasRef, setCanvseRef] = useCustomRef<HTMLCanvasElement>();
@@ -47,26 +29,19 @@ const EditCardPage = () => {
   const [bgImage, setBgImage] = useState<Image | null>(null);
   const [step, setStep] = useState<'colorStep' | 'textStep'>('colorStep');
 
-  const handleImageColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (colorFilter) {
-      colorFilter.color = e.target.value;
-      bgImage?.applyFilters();
-      canvas?.requestRenderAll();
-    }
-  };
-
   const handleColorChange = (color: string) => {
+    if (canvas === null) {
+      return;
+    }
     // if (selectedTemplate?.type === 'image') {
     if ((selectedTemplate as any).id) {
       if (colorFilter) {
         colorFilter.color = color;
         bgImage?.applyFilters();
-        canvas?.requestRenderAll();
+        canvas.requestRenderAll();
       }
     } else {
-      if (canvas) {
-        canvas.setBackgroundColor(color, canvas.renderAll.bind(canvas));
-      }
+      canvas.setBackgroundColor(color, canvas.renderAll.bind(canvas));
     }
   };
 
@@ -130,46 +105,37 @@ const EditCardPage = () => {
       height: canvasContainerRef.clientHeight
     }));
 
-    const cv = new fabric.Canvas(canvasRef);
+    const cv = new fabric.Canvas(canvasRef, {
+      width: canvasContainerRef.clientWidth,
+      height: canvasContainerRef.clientHeight,
+    });
 
-    // ------------------TEMP----------------
-    // fabric.Image.fromURL(`data:image/png;base64,${chairBase64}`, (img) => {
-    //   img.transparentCorners = false;
-    //   img.cornerColor = 'blue';
-    //   img.cornerStyle = 'circle';
-    //   cv.add(img);
-    // });
-    // ------------------TEMP----------------
+    cv.loadFromJSON(canvasDataJSON, () => {
+      // if (selectedTemplate && selectedTemplate.type === 'image') {
+      if (selectedTemplate) {
+        fabric.Image.fromURL(`data:image/png;base64,${(selectedTemplate as any).image}`, (img: Image) => {
+          const colorFilter = new fabric.Image.filters.BlendColor();
+          const brightFilter = new fabric.Image.filters.Brightness();
 
-    cv
-      .setWidth(canvasContainerRef.clientWidth)
-      .setHeight(canvasContainerRef.clientHeight)
-      .loadFromJSON(canvasDataJSON, () => {
-        // if (selectedTemplate && selectedTemplate.type === 'image') {
-        if (selectedTemplate) {
-          fabric.Image.fromURL(`data:image/png;base64,${(selectedTemplate as any).image}`, (img: Image) => {
-            const colorFilter = new fabric.Image.filters.BlendColor();
-            const brightFilter = new fabric.Image.filters.Brightness();
-
-            img.filters = [colorFilter, brightFilter];
-            img.scaleToWidth(cv.width!);
-            img.scaleToHeight(cv.height!);
-            img.originX = 'left';
-            img.originY = 'top';
-            cv.setBackgroundImage(img, cv.renderAll.bind(cv));
-            setColorFilter(colorFilter);
-            setBgImage(img);
-          });
-        }
-
-        cv.on('selection:created', handleObjectSelect);
-        cv.on('selection:updated', handleObjectSelect);
-        cv.on('selection:cleared', () => {
-          setSelectedTextObject(null);
-          setSelectedSvgObject(null);
+          img.filters = [colorFilter, brightFilter];
+          img.scaleToWidth(cv.width!);
+          img.scaleToHeight(cv.height!);
+          img.originX = 'left';
+          img.originY = 'top';
+          cv.setBackgroundImage(img, cv.renderAll.bind(cv));
+          setColorFilter(colorFilter);
+          setBgImage(img);
         });
-        setCanvasObject(cv);
+      }
+
+      cv.on('selection:created', handleObjectSelect);
+      cv.on('selection:updated', handleObjectSelect);
+      cv.on('selection:cleared', () => {
+        setSelectedTextObject(null);
+        setSelectedSvgObject(null);
       });
+      setCanvasObject(cv);
+    });
   }, [token, canvasRef, canvasContainerRef, selectedMarket, canvasDataJSON]);
 
   const handleDownload = () => {
